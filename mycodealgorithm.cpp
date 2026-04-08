bc80c6b949888065a2f8866b15cb842e5e66d15b
Fahim Faisal Pathan <vubon2005@gmail.com>
2026-04-07 20:18:52 -0700

    Add files via upload

---
+++ mycodealgorithm.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selectSort(int array[], int n)
+{
+    int i, j, min_index;
+    for(i=0; i<n-1; i++){
+        min_index = i;
+        for(j=i+1; j<n; j++){
+            if(array[j] < array[min_index]){
+                min_index = j;
+            }
+        }
+        if(min_index != i)
+            swap(array[min_index], array[i]);
+    }
+}
+void printArray(int array[], int size){
+    for(int i=0; i<size; i++){
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+int main(){
+    int arr[]={64,25,12,22,11};
+    int n = 5;
+    selectSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
